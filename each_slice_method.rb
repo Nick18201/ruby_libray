1f72f8f29a049776ad3082f6c4f15a4e80ce99b4
nicolas blum <nblumferracci@gmail.com>
2020-10-23 23:18:15 +0200

    Reorganize an array into slices of n elements, and sort

---
+++ each_slice_method.rb
@@ -0,0 +1,11 @@
+def sorted_pairs(array)
+  result = []
+  # TODO: Reorganize an array into slices of 2 elements, and sort each slice alphabetically.
+  #       You should use of Enumerable#each_slice
+  array.each_slice(2) { |s| result << s.sort }
+  result
+end
+
+# sorted = %w[g d s r w u]
+# p sorted_pairs(sorted)
+# output =>  %W[d g] %W[r s] %W[u w]
