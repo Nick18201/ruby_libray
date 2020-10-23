f7cabb180ceb3fc5cac65fe705f03167adf2114e
nicolas blum <nblumferracci@gmail.com>
2020-10-24 01:38:24 +0200

    map method to transform eahc element of an array, the same way

--- map_method.rb
+++ map_method.rb
@@ -1,3 +1,4 @@
+# Eg 1)
 def add_bang(array)
   # TODO: Take an array of strings and return a new array with "!" appended to each string.
   #       You should use Enumerable#map
@@ -7,3 +8,24 @@ end
 # mapped = %w[bob dhd ssss eerere]
 # p add_bang(mapped)
 # output => bob! ....
+###############################################################################
+
+# eg 2)
+fruits = ["orange", "banane", "ananas", "mangue", "apple"]
+# OUTPUT ["Orange !", "Banane !", "Ananas !", "Mangue !", "Apple !"]
+
+transformed_fruits = []
+
+transformed_fruits_with_map = fruits.map do |fruit|
+
+  # FruitsAPI
+  # url = "fruitsapi.com/#{fruit}"
+  # data = open(url)
+  # data.history
+
+  puts "We are about to transform our fruit: #{fruit}"
+
+  "#{fruit.capitalize} !"
+end
+
+p transformed_fruits_with_map # => ["Orange !", "Banane !", "Ananas !", "Mangue !", "Apple !"]
